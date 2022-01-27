0ad89f9819d5036b6e2e1abbd7f876f3a8f2eb51
shivyucel <72388340+shivyucel@users.noreply.github.com>
2022-01-27 10:08:10 +0000

    commenting_code

--- infection delay/reorient_infection_delay_tables.py
+++ infection delay/reorient_infection_delay_tables.py
@@ -2,32 +2,18 @@ import os
 from multiprocessing import Pool
 import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.rc('figure', dpi=200)
 import glob
-import pandas as pd
-import seaborn as sns
 
-from scipy.cluster.hierarchy import fcluster
-
-from tslearn.clustering import TimeSeriesKMeans
 
 import sys
 data_path = '/Users/shivyucel/Documents/Github.nosync/SDS_2020-2021/SDS_Thesis/Data/h3/'
 
-#populations = np.loadtxt(data_path + 'SIR/v1_populations.txt')
-#
-#populations = pd.Series(populations)
-#
-#
-#top_1000 = populations.sort_values(ascending=False).index
-#
-#
-#top_1000_files = [data_path + f'SIR/time_saved/DPED/new_real_reduction/all/{x}.csv' for x in top_1000]
-file_list = glob.glob('/Volumes/harddrive/Data/v2_uniform_infection_delays/*')
+#file path to all 2599 infection delay tables
+file_directory = ''
+file_list = glob.glob(file_directory)
 
 
+#get dictionary with all 2599 tables loaded
 new_dic = {}
 for file in file_list:
     ts = pd.read_csv(file, index_col = 'Unnamed: 0')
@@ -39,42 +25,25 @@ counter = {}
 time_dic = {}
 count = 0 
 
+#get all region IDs
 all_locations = pd.Series(new_dic[file_list[0]].columns).append(pd.Series(new_dic[file_list[1]].columns))
 all_locations.drop_duplicates(inplace=True)
 
+#concatenate every region's infection delays across all outbreak scenarios
+#iterate across dictionary to find all of a region's infection delay curves and turn them into one table
+output_directory = ''
 def get_all_time_saved(location):
     loc_df = pd.DataFrame()
     for key in list(new_dic.keys()):
         if key.split('/')[-1][:-4] != location:
-            #try:
             to_concat = pd.DataFrame(new_dic[key][location])
             to_concat.columns =  [key.split('/')[-1][:-4]]
             loc_df = pd.concat([loc_df, to_concat], axis=1)
-            #except:
-             #   continue
-    loc_df.to_csv(f'/Volumes/harddrive/Data/v2_uniform_concat/{location}.csv')
 
+    loc_df.to_csv(output_directory + f'{str(location)}.csv')
 
-current = glob.glob('/Volumes/harddrive/Data/v2_uniform_concat/*')
-current.sort()
+#takes multiple hours to run
 
-#current = pd.Series(current)
-#current = current.str.split('/')
-#current = [x[-1] for x in current]
-#current = [x.split('.') for x in current]
-#current = [x[0] for x in current]
-
-allnums = pd.Series(np.arange(0, 2599)).astype(str)
-to_do = [str(x) for x in allnums.values if x not in current]
-#to_do = [f'/Volumes/harddrive/Data/v2_uniform_infection_delays/{x}.csv' for x in to_do]
-
-for location in to_do:
+for location in all_locations:
     get_all_time_saved(location)
     
-#all_files =   glob.glob
-#from multiprocessing import Pool
-#
-#
-#if __name__ == '__main__':
-#    with Pool(8) as p:
-#        print(p.map(get_all_time_saved, all_locations.values))
